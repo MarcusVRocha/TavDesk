3c9d85afb699c07a7e9745ed40f5f0ca401122b6
MarcusVRocha <marcus.cavalcanterocha@gmail.com>
2026-03-23 14:56:17 -0300

    chore: inicializa projeto Spring Boot do Tavdesk

---
+++ src/main/java/com/marcusvrocha/TavDesk/TavDeskApplication.java
@@ -0,0 +1,13 @@
+package com.marcusvrocha.TavDesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TavDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TavDeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcusvrocha/TavDesk/TavDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcusvrocha.TavDesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TavDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
