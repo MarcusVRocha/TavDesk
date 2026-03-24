ae5a9fda02802b3c55659ef30129c315a2989b26
MarcusVRocha <marcus.cavalcanterocha@gmail.com>
2026-03-24 12:03:52 -0300

    fix: Corrigindo o JpaRepository

--- src/main/java/com/marcusvrocha/TavDesk/repository/ChamadoRepository.java
+++ src/main/java/com/marcusvrocha/TavDesk/repository/ChamadoRepository.java
@@ -1,6 +1,7 @@
 package com.marcusvrocha.TavDesk.repository;
 
+import com.marcusvrocha.TavDesk.model.Chamado;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChamadoRepository extends JpaRepository<ChamadoRepository, Long> {
+public interface ChamadoRepository extends JpaRepository<Chamado, Long> {
 }
