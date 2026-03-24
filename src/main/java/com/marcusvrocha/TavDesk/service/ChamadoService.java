c6882164c4afe7220f7f4d04623a2a3266ddd981
MarcusVRocha <marcus.cavalcanterocha@gmail.com>
2026-03-24 12:06:42 -0300

    feat: implementa o ChamadoService com as regras de criação e listagem de chamados

---
+++ src/main/java/com/marcusvrocha/TavDesk/service/ChamadoService.java
@@ -0,0 +1,30 @@
+package com.marcusvrocha.TavDesk.service;
+
+import com.marcusvrocha.TavDesk.enums.StatusChamado;
+import com.marcusvrocha.TavDesk.model.Chamado;
+import com.marcusvrocha.TavDesk.repository.ChamadoRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class ChamadoService {
+
+    private final ChamadoRepository chamadoRepository;
+
+    public ChamadoService(ChamadoRepository chamadoRepository) {
+        this.chamadoRepository = chamadoRepository;
+    }
+
+    public Chamado criarChamado(Chamado chamado) {
+        chamado.setStatus(StatusChamado.ABERTO);
+        chamado.setDataCriacao(LocalDateTime.now());
+        return chamadoRepository.save(chamado);
+    }
+
+    public List<Chamado> listarChamados() {
+        return chamadoRepository.findAll();
+    }
+
+}
