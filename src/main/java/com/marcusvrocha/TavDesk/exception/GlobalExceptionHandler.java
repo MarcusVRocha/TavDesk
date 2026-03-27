d9f8b4842612acbd609d4cc69661a26604b34ab7
MarcusVRocha <marcus.cavalcanterocha@gmail.com>
2026-03-27 16:06:41 -0300

    feat: adiciona tratamento para o status 400

--- src/main/java/com/marcusvrocha/TavDesk/exception/GlobalExceptionHandler.java
+++ src/main/java/com/marcusvrocha/TavDesk/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.marcusvrocha.TavDesk.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -21,5 +22,22 @@ public class GlobalExceptionHandler {
         erro.put("Mensagem:", ex.getMessage());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+
+        Map<String, Object> erros = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                erros.put(error.getField(), error.getDefaultMessage())
+        );
+
+        Map<String, Object> resposta = new HashMap<>();
+        resposta.put("Data/Hora", LocalDateTime.now());
+        resposta.put("Status:", 400);
+        resposta.put("Erro!", "Bad Request.");
+        resposta.put("Erros:", erros);
+
+        return ResponseEntity.badRequest().body(resposta);
     }
 }
