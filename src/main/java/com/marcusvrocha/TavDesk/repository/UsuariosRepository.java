dc6d5bb5dccc6ab71ac50b5f0b3c94f5cad58cc0
MarcusVRocha <marcus.cavalcanterocha@gmail.com>
2026-04-03 19:35:07 -0300

    refactor: inclui Optional para futura validação por e-mail

--- src/main/java/com/marcusvrocha/TavDesk/repository/UsuariosRepository.java
+++ src/main/java/com/marcusvrocha/TavDesk/repository/UsuariosRepository.java
@@ -3,5 +3,11 @@ package com.marcusvrocha.TavDesk.repository;
 import com.marcusvrocha.TavDesk.model.Usuarios;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuariosRepository extends JpaRepository<Usuarios, Long> {
+    Optional<Usuarios> findbyEmail(String email);
+
+    boolean existsByEmail(String email);
+
 }
