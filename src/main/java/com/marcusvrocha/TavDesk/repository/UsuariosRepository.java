45f46a3628ffa2ce7ca000a962ca546dc0e9f2d2
MarcusVRocha <marcus.cavalcanterocha@gmail.com>
2026-04-08 19:37:18 -0300

    fix: corrigindo o método findByEmail

--- src/main/java/com/marcusvrocha/TavDesk/repository/UsuariosRepository.java
+++ src/main/java/com/marcusvrocha/TavDesk/repository/UsuariosRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UsuariosRepository extends JpaRepository<Usuarios, Long> {
-    Optional<Usuarios> findbyEmail(String email);
+    Optional<Usuarios> findByEmail(String email);
 
     boolean existsByEmail(String email);
 
